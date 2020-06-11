85057a482c06ba66725106905b3e203b2ca84f87
Hu, Xiaohui <xiaohuhu@stubhub.com>
2020-06-12 00:28:47 +0800

    web api 使用域名

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -3,10 +3,13 @@ export const Constants = {
     
     // APIBaseUrl: process.env.NODE_ENV === "production"? "http://106.75.216.135:8004/" : "https://localhost:44362/",
     // ResourceUrl: process.env.NODE_ENV === "production"? "http://app.kangfupanda.com/Upload/" : "https://localhost:44362/Upload/",
-    APIBaseUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/" : "https://localhost:44362/",
+    //APIBaseUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/" : "https://localhost:44362/",
+    APIBaseUrl: process.env.NODE_ENV === "production"? "https://api.kangfupanda.com/" : "https://localhost:44362/",
     //ResourceUrl: process.env.NODE_ENV === "production"? "http://app.kangfupanda.com/Upload/" : "https://localhost:44362/Upload/",
-    ResourceUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/Upload/" : "https://localhost:44362/Upload/",
+    //ResourceUrl: process.env.NODE_ENV === "production"? "http://47.111.166.154:8004/Upload/" : "https://localhost:44362/Upload/",
+    ResourceUrl: process.env.NODE_ENV === "production"? "https://api.kangfupanda.com/Upload/" : "https://localhost:44362/Upload/",
     AppId:'wxc10ff63ecf588c90',
     AppSecret: '7289786fae8511cbe9e94a8aec93e125',
     RedirectUrl: 'http://app.kangfupanda.com/#/profile',    
 }
+
